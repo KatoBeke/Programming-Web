bbd009f672ac45abc36b93547e0d704320fc90d6
Kato Beke <beke.kato@telenet.be>
2021-12-25 23:07:26 +0100

    next commit

--- src/components/Graps.vue
+++ src/components/Graps.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div>
-            <column-chart :data="items" thousands="." xtitle="Song" ytitle="Aantal votes" :max="2500" width="40%" :colors="['#006ca2']" :messages="{empty: 'Geen data'}" ></column-chart>
+            <column-chart :data="items" thousands="." xtitle="Song" ytitle="Aantal votes" :max="2500" width="60%" :colors="['#006ca2']" :messages="{empty: 'Geen data'}" ></column-chart>
         </div>
     </div>
 </template>
